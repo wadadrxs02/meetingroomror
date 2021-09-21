77cf1a09da0e55952ca7f685be1f96ae7a9b8f64
wadadrxs02 <nurawadah.ismail@gmail.com>
2021-09-21 16:35:10 +0800

    calendar ok dah

--- db/migrate/20210831191820_create_bookings.rb
+++ db/migrate/20210831191820_create_bookings.rb
@@ -2,8 +2,8 @@ class CreateBookings < ActiveRecord::Migration[6.1]
   def change
     create_table :bookings do |t|
       t.string :title
-      t.time :start_time
-      t.time :end_time
+      t.datetime :start_time
+      t.datetime :end_time
       t.integer :pax
       t.references :admin, null: false, foreign_key: true
       t.references :meetingroom, null: false, foreign_key: true
